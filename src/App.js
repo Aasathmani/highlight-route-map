2b73c54d0b366c6ae05a16bc8c757a88641eca18
Aasath M <aasath.m@thinkpalm.com>
2024-05-07 16:39:25 +0530

    the route map highlight

--- src/App.js
+++ src/App.js
@@ -1,25 +1,99 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef, useState } from "react";
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
 
-function App() {
+const App = () => {
+  const mapRef = useRef(null);
+  const [selectedRoute, setSelectedRoute] = useState(null);
+
+  useEffect(() => {
+    const routes = [
+      [
+        [40, -100],
+        [42, -98],
+        [38, -95],
+      ],
+      [
+        [35, -110],
+        [37, -108],
+        [33, -105],
+      ],
+      [
+        [45, -115],
+        [43, -112],
+        [41, -110],
+      ],
+      [
+        [32, -120],
+        [34, -118],
+        [36, -115],
+      ],
+      [
+        [48, -100],
+        [46, -98],
+        [44, -95],
+      ],
+      [
+        [30, -105],
+        [32, -103],
+        [34, -100],
+      ],
+      [
+        [38, -115],
+        [36, -113],
+        [34, -110],
+      ],
+      [
+        [47, -120],
+        [45, -118],
+        [43, -115],
+      ],
+      [
+        [37, -100],
+        [39, -98],
+        [41, -95],
+      ],
+      [
+        [42, -105],
+        [44, -103],
+        [46, -100],
+      ],
+    ];
+
+    const map = L.map(mapRef.current, {
+      center: [40, -100],
+      zoom: 5,
+    });
+
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
+
+    routes.map((route, index) => {
+      const polyline = L.polyline(route, {
+        color: selectedRoute === index ? "#ff0000" : "#3388ff",
+        weight: selectedRoute === index ? 5 : 5,
+        opacity: 0.7,
+        clickable: true,
+        distance: 40,
+      }).addTo(map);
+
+      polyline.on("click", () => {
+        setSelectedRoute(index);
+      });
+
+      return polyline;
+    });
+
+    return () => {
+      map.remove();
+    };
+  }, [selectedRoute]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div id="map" ref={mapRef} style={{ height: "100vh", width: "100%" }}></div>
   );
-}
+};
 
 export default App;
